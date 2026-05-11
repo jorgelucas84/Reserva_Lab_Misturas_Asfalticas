c2b7ee2a6bcc1130a6cd3b43944e1d8b46a4e1d5
jorgelucas84 <128716172+jorgelucas84@users.noreply.github.com>
2026-05-11 09:10:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * FRONTEND COMPLETO - script.js
  */
 
-const URL_API = "https://script.google.com/macros/s/AKfycbwALlnur4XRWMELfuHd5svC1buy3Z-xP8kIqw9FrIZZlMUxQBzxQ8RslN8KEpqBurrCWg/exec";
+const URL_API = "https://script.google.com/macros/s/AKfycbxERjd9XUkR69pg6QzudRS3Zk9mPkWEQafNpYYKVMBa0a2oKReHppUA8aVyaBzvE85l3A/exec";
 
 const corpoAgenda = document.getElementById('corpo-agenda');
 const seletorData = document.getElementById('data');
